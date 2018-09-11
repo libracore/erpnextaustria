22aaef65b4d847b11741709801f28ea873f9fe19
lasalesi <larsericmueller@gmail.com>
2018-09-11 16:54:48 +0200

    xml wrapper

--- erpnextaustria/erpnextaustria/doctype/at_vat_declaration/at_vat_declaration.py
+++ erpnextaustria/erpnextaustria/doctype/at_vat_declaration/at_vat_declaration.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from datetime import datetime
 
 class ATVATDeclaration(Document):
     # generate xml export
@@ -13,62 +14,55 @@ class ATVATDeclaration(Document):
         # create xml header
         content = make_line("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
         # define xml root node
-        content += make_line("<AbaConnectContainer>")
-        # control counter (TaskCount is actually the number of transactions)
-        transaction_count = 0
-        transaction_count_identifier = "<!-- $COUNT -->"
-        content += make_line(" <TaskCount>{0}</TaskCount>".format(transaction_count_identifier))
-        # task container
-        content += make_line(" <Task>")
-        # parameters
-        content += make_line("  <Parameter>")
-        content += make_line("   <Application>FIBU</Application>")
-        content += make_line("   <Id>XML Buchungen</Id>")
-        content += make_line("   <MapId>AbaDefault</MapId>")
-        content += make_line("   <Version>2015.00</Version>")
-        content += make_line("  </Parameter>")
-
-        # add payment entry transactions
-        sql_query = """SELECT 
-                      `tabAccount`.`account_number`, 
-                      SUM(`tabGL Entry`.`debit`) AS `debit`, 
-                      SUM(`tabGL Entry`.`credit`) AS `credit`,
-                      `tabGL Entry`.`account_currency` AS `currency`                      
-                    FROM `tabGL Entry`
-                    LEFT JOIN `tabAccount` ON `tabGL Entry`.`account` = `tabAccount`.`name`
-                    WHERE `tabGL Entry`.`posting_date` >= '{start_date}' 
-                      AND `tabGL Entry`.`posting_date` <= '{end_date}'
-                      AND `tabGL Entry`.`docstatus` = 1
-                      AND `tabGL Entry`.`exported_to_abacus` = 0
-                    GROUP BY `tabAccount`.`account_number`;
-            """.format(start_date=start_date, end_date=end_date)
-        items = frappe.db.sql(sql_query, as_dict=True)
-        # mark all entries as exported
-        export_matches = frappe.get_all("GL Entry", filters=[
-            ["posting_date",">=", start_date],
-            ["posting_date","<=", end_date],
-            ["docstatus","=", 1],
-		    ["exported_to_abacus","=",0]], fields=['name'])
-        for export_match in export_matches:
-            record = frappe.get_doc("GL Entry", export_match['name'])
-            record.exported_to_abacus = 1
-            record.save(ignore_permissions=True)
-        for item in items:
-            if item.account_number:
-                if item.credit != 0:
-                    transaction_count += 1        
-                    content += add_transaction_block(item.account_number, item.credit, 
-                        "C", end_date, item.currency, transaction_count)
-                if item.debit != 0:
-                    transaction_count += 1        
-                    content += add_transaction_block(item.account_number, item.debit, 
-                        "D", end_date, item.currency, transaction_count)
-        # add footer
-        content += make_line(" </Task>")
-        content += make_line("</AbaConnectContainer>")
-        # insert control numbers
-        content = content.replace(transaction_count_identifier, "{0}".format(transaction_count))
-        
+		content = make_line("<ERKLAERUNGS_UEBERMITTLUNG>")
+		content = make_line(" <INFO_DATEN>")
+		content = make_line("  <ART_IDENTIFIKATIONSBEGRIFF>FASTNR</ART_IDENTIFIKATIONSBEGRIFF>")
+		fastnr = frappe.get_value("ERPNextAustria Settings", "ERPNextAustria Settings", "fastnr")
+		content = make_line("  <IDENTIFIKATIONSBEGRIFF>{0}</IDENTIFIKATIONSBEGRIFF>".format(fastnr))
+		content = make_line("  <PAKET_NR>1</PAKET_NR>")
+		now = dateime.now()
+		content = make_line("  <DATUM_ERSTELLUNG type=\"datum\">{0:04d}-{1:02d}-{2:02d}</DATUM_ERSTELLUNG>".format(now.year, now.month, now.day))
+		content = make_line("  <UHRZEIT_ERSTELLUNG type=\"uhrzeit\">{0:02d}:{1:02d}:{2:02d}</UHRZEIT_ERSTELLUNG>".format(now.hour, now.minute, now.second))
+		content = make_line("  <ANZAHL_ERKLAERUNGEN>1</ANZAHL_ERKLAERUNGEN>")
+		content = make_line(" </INFO_DATEN>")
+		content = make_line(" <ERKLAERUNG art=\"U30\">")
+		content = make_line("  <SATZNR>1</SATZNR>")
+		content = make_line("  <ALLGEMEINE_DATEN>")
+		content = make_line("   <ANBRINGEN>U30</ANBRINGEN>")
+		content = make_line("   <ZRVON type=\"jahrmonat\">{0}</ZRVON>".format(self.start_date[0:7]))
+		content = make_line("   <ZRBIS type=\"jahrmonat\">{0}</ZRBIS>".format(self.end_date[0:7]))
+		content = make_line("   <FASTNR>{0}</FASTNR>".format(fastnr))
+		content = make_line("   <KUNDENINFO>{0}</KUNDENINFO>".format(self.company))
+		content = make_line("  </ALLGEMEINE_DATEN>")
+		content = make_line("  <LIEFERUNGEN_LEISTUNGEN_EIGENVERBRAUCH>")
+		content = make_line("   <KZ000 type=\"kz\">{0}</KZ000>".format(self.revenue))
+		content = make_line("   <STEUERFREI>")
+		content = make_line("    <KZ011 type=\"kz\">{0}</KZ011>".format(self.exports))
+		content = make_line("	 <KZ017 type=\"kz\">{0}</KZ017>".format(self.inner_eu))
+		content = make_line("   </STEUERFREI>")
+		content = make_line("   <KZ021 type=\"kz\">{0}</KZ021>".format(self.receiver_vat))
+		content = make_line("   <VERSTEUERT>")
+		content = make_line("    <KZ022 type=\"kz\">{0}</KZ022>".format(self.amount_normal))
+		content = make_line("	 <KZ029 type=\"kz\">{0}</KZ029>".format(self.reduced_amount))
+		content = make_line("	 <KZ057 type=\"kz\">{0}</KZ057>".format(self.tax_057))
+		content = make_line("   </VERSTEUERT>")
+		content = make_line("  </LIEFERUNGEN_LEISTUNGEN_EIGENVERBRAUCH>")
+		content = make_line("  <VORSTEUER>")
+		content = make_line("   <KZ060 type=\"kz\">{0}</KZ060>".format(self.total_pretax))
+		content = make_line("   <KZ061 type=\"kz\">{0}</KZ061>".format(self.import_pretax))
+		content = make_line("   <KZ083 type=\"kz\">{0}</KZ083>".format(self.import_charge_pretax))
+		content = make_line("   <KZ065 type=\"kz\">{0}</KZ065>".format(self.intercommunal_pretax))
+		content = make_line("   <KZ066 type=\"kz\">{0}</KZ066>".format(self.taxation_pretax))
+		content = make_line("  </VORSTEUER>")
+		content = make_line("  <INNERGEMEINSCHAFTLICHE_ERWERBE>")
+		content = make_line("   <KZ070 type=\"kz\">{0}</KZ070>".format(self.intercommunal_revenue))
+		content = make_line("   <VERSTEUERT_IGE>")
+		content = make_line("    <KZ072 type=\"kz\">{0}</KZ072>".format(self.amount_inter_normal))
+		content = make_line("   </VERSTEUERT_IGE>")
+		content = make_line("  </INNERGEMEINSCHAFTLICHE_ERWERBE>")
+		content = make_line(" </ERKLAERUNG>")
+		content = make_line("</ERKLAERUNGS_UEBERMITTLUNG>")
+     
         return { 'content': content }
     #except IndexError:
     #    frappe.msgprint( _("Please select at least one payment."), _("Information") )
