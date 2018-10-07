66aaa8a0627cf62765c04f5e4431c0c08fdd201f
lasalesi <larsericmueller@gmail.com>
2018-10-07 20:37:14 +0200

    cleanup of code

--- erpnextaustria/erpnextaustria/utils.py
+++ erpnextaustria/erpnextaustria/utils.py
@@ -3,11 +3,14 @@
 # For license information, please see license.txt
 
 # imports
+import frappe
 from zeep import Client
 
 # UID validation
+#
+# Returns a dict with the attribute 'valid' = "True" or "False"
+@frappe.whitelist()
 def check_uid(uid):
     client = Client('http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl')
-    result = client.service.checkVat('AT', 'U36401407')
-    print(result.valid)
+    result = client.service.checkVat(uid[0:2], uid[2:])
     return result.valid
