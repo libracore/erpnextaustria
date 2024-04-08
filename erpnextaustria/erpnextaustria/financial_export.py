6948cc663a308cd2fdc051d1fb9494078d71d260
lasalesi <larsericmueller@gmail.com>
2024-04-08 10:42:21 +0200

    balance export functions

--- erpnextaustria/erpnextaustria/financial_export.py
+++ erpnextaustria/erpnextaustria/financial_export.py
@@ -23,6 +23,7 @@ def create_financial_export(fiscal_year, company):
     acts_sheet_file = create_account_sheet_file(fiscal_year, company)
     journal_file = create_journal_file(fiscal_year, company)
     dbt_crt_balance_file = create_debtors_creditors_balance_file(fiscal_year, company)
+    act_balance_file = create_account_balance_file(fiscal_year, company)
     
     # create zip archie
     
@@ -294,55 +295,41 @@ def create_debtors_creditors_balance_file(fiscal_year, company):
     # prepare debtors
     debtors = frappe.get_all("Customer", fields=['name', 'customer_name', 'tax_id', 'payment_terms'])
     for d in debtors:
+        balance = get_party_balances(d.name, company, fiscal_year)
+        opening_balance = balance['opening_debit'] - balance['opening_credit']
+        period_change = balance['total_debit'] - balance['total_credit']
         record = {
             'account': make_safe_string(d.name),
             'name': make_safe_string(d.customer_name),
-            'uid': make_safe_string(d.tax_id),
-            'payment_terms': make_safe_string(d.payment_terms)
+            'opening_balance': opening_balance,
+            'total_debit': balance['opening_debit'],
+            'total_crdit': (-1) * balance['opening_credit'],
+            'balance_debit': opening_balance + period_change if period_change >= 0 else None,
+            'balance_credit': opening_balance + period_change if period_change < 0 else None,
         }
-        if d.payment_terms:
-            record.update({
-                'skonto_days_1': frappe.get_cached_value("Payment Terms Template", d.payment_terms, 'skonto_days'),
-                'skonto_percent_1': frappe.get_cached_value("Payment Terms Template", d.payment_terms, 'skonto_percent')
-            })
-        address = get_primary_customer_address(d.name)
-        if address:
-            record.update({
-                'street': make_safe_string(address.address_line1),
-                'pincode': make_safe_string(address.pincode),
-                'city': make_safe_string(address.city),
-                'country': make_safe_string(address.country)
-            })
             
         data.append(record)
     
     # prepare creditors
     creditors = frappe.get_all("Supplier", fields=['name', 'supplier_name', 'tax_id', 'payment_terms'])
     for c in creditors:
+        balance = get_party_balances(c.name, company, fiscal_year)
+        opening_balance = balance['opening_debit'] - balance['opening_credit']
+        period_change = balance['total_debit'] - balance['total_credit']
         record = {
             'account': make_safe_string(c.name),
-            'name': make_safe_string(c.supplier_name),
-            'uid': make_safe_string(c.tax_id),
-            'payment_terms': make_safe_string(c.payment_terms)
+            'name': make_safe_string(c.customer_name),
+            'opening_balance': opening_balance,
+            'total_debit': balance['opening_debit'],
+            'total_crdit': (-1) * balance['opening_credit'],
+            'balance_debit': opening_balance + period_change if period_change >= 0 else None,
+            'balance_credit': opening_balance + period_change if period_change < 0 else None,
         }
-        if c.payment_terms:
-            record.update({
-                'skonto_days_1': frappe.get_cached_value("Payment Terms Template", c.payment_terms, 'skonto_days'),
-                'skonto_percent_1': frappe.get_cached_value("Payment Terms Template", c.payment_terms, 'skonto_percent')
-            })
-        address = get_primary_supplier_address(c.name)
-        if address:
-            record.update({
-                'street': make_safe_string(address.address_line1),
-                'pincode': make_safe_string(address.pincode),
-                'city': make_safe_string(address.city),
-                'country': make_safe_string(address.country)
-            })
             
         data.append(record)
         
     # render template
-    output_debtors_creditors = frappe.render_template("erpnextaustria/templates/xml/debtors_creditors_export.html", 
+    output_debtors_creditors = frappe.render_template("erpnextaustria/templates/xml/debtors_creditors_balance_export.html", 
         {
             'fiscal_year': frappe.get_doc("Fiscal Year", fiscal_year),
             'company': company,
@@ -357,32 +344,132 @@ def create_debtors_creditors_balance_file(fiscal_year, company):
     f.close()
     return filename
 
+
+def create_account_balance_file(fiscal_year, company):
+    data = []
+    # prepare debtors
+    accounts = frappe.get_all("Account", 
+        filters={'company': company},
+        fields=['name', 'account_name', 'account_number', 'root_type', 'account_type'],
+        order_by='account_number')
+    for a in accounts:
+        if a.account_number:                            # only export with account numbers (leave structural elements)
+            balance = get_party_balances(a.name, company, fiscal_year)
+            opening_balance = balance['opening_debit'] - balance['opening_credit']
+            period_change = balance['total_debit'] - balance['total_credit']
+            record = {
+                'account': make_safe_string(a.account_number),
+                'name': make_safe_string(a.account_name),
+                'opening_balance': opening_balance,
+                'total_debit': balance['opening_debit'],
+                'total_crdit': (-1) * balance['opening_credit'],
+                'balance_debit': opening_balance + period_change if period_change >= 0 else None,
+                'balance_credit': opening_balance + period_change if period_change < 0 else None,
+            }
+            data.append(record)
+        
+    # render template
+    output_debtors_creditors = frappe.render_template("erpnextaustria/templates/xml/account_balance_export.html", 
+        {
+            'fiscal_year': frappe.get_doc("Fiscal Year", fiscal_year),
+            'company': company,
+            'data': data
+        }
+    )
+    
+    # write file
+    filename = "/tmp/ACL_Saldenliste_{0}_{1}.csv".format(company, fiscal_year)
+    f = open(filename, "w", encoding="utf-8")              # cp1252 would be required, but cannot encode all required chars
+    f.write(output_debtors_creditors)
+    f.close()
+    return filename
+    
 def get_party_balances(party, company, fiscal_year):
-    party_balance = frappe.db.sql("""
+    opening_party_balance = frappe.db.sql("""
             SELECT
-                `name`,
-                `posting_date`,
                 `account`,
                 `against`,
-                `voucher_type`,
-                `voucher_no`,
-                `remarks`,
-                `debit`,
-                `credit`,
-                `creation`
+                IFNULL(SUM(`debit`), 0) AS `total_debit`,
+                IFNULL(SUM(`credit`), 0)  AS `total_credit`
+            FROM `tabGL Entry`
+            WHERE
+                `company` = "{company}"
+                AND `posting_date` < "{from_date}"
+                AND `against` = "{party}";
+        """.format(
+            company=company, 
+            from_date=frappe.get_value("Fiscal Year", fiscal_year, "year_start_date"), 
+            to_date=frappe.get_value("Fiscal Year", fiscal_year, "year_end_date"),
+            party=party
+        ), as_dict=True)
+        
+    party_balance = frappe.db.sql("""
+            SELECT
+                `account`,
+                `against`,
+                IFNULL(SUM(`debit`), 0) AS `total_debit`,
+                IFNULL(SUM(`credit`), 0)  AS `total_credit`
             FROM `tabGL Entry`
             WHERE
                 `company` = "{company}"
                 AND `posting_date` BETWEEN "{from_date}" AND "{to_date}"
-            ORDER BY `name` ASC
+                AND `against` = "{party}";
         """.format(
             company=company, 
             from_date=frappe.get_value("Fiscal Year", fiscal_year, "year_start_date"), 
-            to_date=frappe.get_value("Fiscal Year", fiscal_year, "year_end_date")
+            to_date=frappe.get_value("Fiscal Year", fiscal_year, "year_end_date"),
+            party=party
         ), as_dict=True)
+        
     return {
-        'total_debit': 0,
-        'total_debit': 0
+        'opening_debit': opening_party_balance[0]['total_debit'],
+        'opening_credit': opening_party_balance[0]['total_credit'],
+        'total_debit': party_balance[0]['total_debit'],
+        'total_credit': party_balance[0]['total_credit'],
     }
+
+def get_account_balances(account, company, fiscal_year):
+    opening_party_balance = frappe.db.sql("""
+            SELECT
+                `account`,
+                IFNULL(SUM(`debit`), 0) AS `total_debit`,
+                IFNULL(SUM(`credit`), 0)  AS `total_credit`
+            FROM `tabGL Entry`
+            WHERE
+                `company` = "{company}"
+                AND `posting_date` < "{from_date}"
+                AND `account` = "{account}";
+        """.format(
+            company=company, 
+            from_date=frappe.get_value("Fiscal Year", fiscal_year, "year_start_date"), 
+            to_date=frappe.get_value("Fiscal Year", fiscal_year, "year_end_date"),
+            account=account
+        ), as_dict=True)
+        
+    party_balance = frappe.db.sql("""
+            SELECT
+                `account`,
+                `against`,
+                IFNULL(SUM(`debit`), 0) AS `total_debit`,
+                IFNULL(SUM(`credit`), 0)  AS `total_credit`
+            FROM `tabGL Entry`
+            WHERE
+                `company` = "{company}"
+                AND `posting_date` BETWEEN "{from_date}" AND "{to_date}"
+                AND `account` = "{account}";
+        """.format(
+            company=company, 
+            from_date=frappe.get_value("Fiscal Year", fiscal_year, "year_start_date"), 
+            to_date=frappe.get_value("Fiscal Year", fiscal_year, "year_end_date"),
+            account=account
+        ), as_dict=True)
+        
+    return {
+        'opening_debit': opening_party_balance[0]['total_debit'],
+        'opening_credit': opening_party_balance[0]['total_credit'],
+        'total_debit': party_balance[0]['total_debit'],
+        'total_credit': party_balance[0]['total_credit'],
+    }
+
 def make_safe_string(s):
     return (s or "").replace(";", " ").replace("\n", " ").replace("\r", " ").replace("\"", "")
