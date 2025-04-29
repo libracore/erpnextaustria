fd587d6eea601192e5b362f573db20f7aabc7ec8
Lars <lars.mueller@libracore.com>
2025-04-29 18:51:41 +0000

    update version

--- erpnextaustria/erpnextaustria/report/summary_message/summary_message.py
+++ erpnextaustria/erpnextaustria/report/summary_message/summary_message.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017-2018, libracore and contributors
+# Copyright (c) 2017-2025, libracore and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
